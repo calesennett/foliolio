dc0aaef029942575244f46928d9a3b568a19a14f
Cale Sennett <calesennett@gmail.com>
2022-03-25 12:41:11 -0700

    add chrome-aws-lambda for vercel puppeteer support

--- pages/api/screenshot.js
+++ pages/api/screenshot.js
@@ -1,9 +1,15 @@
 import captureWebsite from 'capture-website'
+import chromium       from 'chrome-aws-lambda'
 
 export default async function handler(req, res) {
   const {url}          = req.body
   const screenshotData = await captureWebsite.base64(url, {
-    type: 'jpeg'
+    type: 'jpeg',
+    ...(process.env.NODE_ENV === 'production') && {launchOptions: {
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless
+    }}
   })
 
   res.json({screenshot: screenshotData})
